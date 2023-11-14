68dd66cc05b062cf4d61a4cf324a9766ca9ffbec
Nisrine2001 <nisrine.touijer@gmail.com>
2023-11-14 14:28:51 -0800

    header

--- shell.h
+++ shell.h
@@ -17,4 +17,11 @@
 
 char *read_line(void);
 
+char *_strdup(const char *str);
+int _strcmp(char *s1, char *s2);
+int strlen(char *s);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+
+
 #endif
