5c098bfa61d64a65551bd95b681bfe57e928deed
hajar <makrinihajar@gmail.com>
2023-11-14 13:41:38 -0800

    task0 is done

---
+++ task_0.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - it displays a sentence with printf .
+ *
+ * Return: Always 0 (Success)
+ */
+main()
+{
+	printf("we will start a new task so enter a number please\n");
+}
+
