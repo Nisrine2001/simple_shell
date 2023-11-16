377ef8be6acd20c68641e8524e61bb833ab7ae67
hajar <makrinihajar@gmail.com>
2023-11-15 20:23:51 -0800

    hello

--- task6.c
+++ task6.c
@@ -4,13 +4,13 @@
  *
  * Description: A C program that works in the same way as getline.
  * Return:Always(0) (Success). 
+ *
  */
 int main(void)
 {
 char buf[200];
 ssize_t nd;
-size_t len = 0
-printf("Enter a line of text:\n");
+size_t len = 0;
 while ((nd = read(STDIN_FILENO, buf + len, sizeof(buf) - len - 1)) > 0)
 {len = len + nd;
 if (buf[len - 1] == '\n')
